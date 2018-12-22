f8b283f1ab389679c053da65bacb1fe0fa9fd5fc
ashok <asho.info@gmail.com>
2018-12-22 20:48:48 +0530

    test GET /todos route

--- server/server.js
+++ server/server.js
@@ -7,18 +7,26 @@ var {Users} = require('./models/user');
 
 var app = express();
 app.use(bodyParser.json());
-app.post('/todos',(req,res) =>{
-// console.log(req.body);
-var newTodo = new Todo({
-       text:req.body.text
- });
+
+
+app.post('/todos', (req, res) => {
+    // console.log(req.body);
+    var newTodo = new Todo({
+        text: req.body.text
+    });
     newTodo.save().then((doc) => {
         res.send(doc);
     }, (e) => {
         res.status(400).send(e);
-
     });
+});
 
+app.get('/todos',(req,res) =>{
+    Todo.find().then((todos) =>{
+        res.send(todos);
+    },(e) =>{
+        res.status(400).send(e);
+    });
 });
 
 app.listen(3000,()=>{
